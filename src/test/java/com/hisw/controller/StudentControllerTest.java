292c40cc374752aaa831ed588fa2d2bffa11b375
swallow <icbczjyy@gmail.com>
2016-08-30 11:43:41 +0800

    Revert "upload"
    This reverts commit f09324ec35cacc0ce5bf941a59caeb12d35bd9b4.

--- src/test/java/com/hisw/controller/StudentControllerTest.java
+++
@@ -1,9 +0,0 @@
-//package com.hisw.controller;
-//
-//import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-//
-///**
-// * Created by swallow on 2016/8/4.
-// */
-//public class StudentControllerTest extends AbstractJUnit4SpringContextTests {
-//}
